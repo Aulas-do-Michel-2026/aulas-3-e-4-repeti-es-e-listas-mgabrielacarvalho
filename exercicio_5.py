fd180d6551c4f23897e9c7ed57bf0a36cd56b5d6
mgabrielacarvalho <carvalho.mariagabriela@gmail.com>
2026-06-18 18:45:51 -0300

    Refactor organism list and calculate highest average
    Refactor the list of organisms for better readability and calculate the organism with the highest average.

--- exercicio_5.py
+++ exercicio_5.py
@@ -36,7 +36,30 @@ Dica: Utilize mais de um for para resolver o exercício, um para a lista de orga
 O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 """
 
-# Lista
-lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
-# Fazer a partir daqui
+
+lista_de_organismos = [[50, 50, 50], 
+                       [125, 99, 12], 
+                       [19, 91, 42], 
+                       [40, 189, 0], 
+                       [1, 0, 0], 
+                       [100, 100, 70], 
+                       [99, 12, 12]
+                       ]
+
+maior_media = 0
+posicao_maior_media = [0]
+
+for i, organismo in enumerate(lista_de_organismos):
+    soma = 0
+
+    for leitura in organismo:
+        soma = soma + leitura
+    
+    media = soma/len(organismo)
+
+if media > maior_media:
+    maior_media = media
+    posicao_maior_media = i
+
+print(f'O organismo com maior média é o da posição {posicao_maior_media} da lista.')
