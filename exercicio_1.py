7648dd06264457b00cb7b2118ce2cfd63b5543ed
mgabrielacarvalho <carvalho.mariagabriela@gmail.com>
2026-06-18 17:06:23 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -18,3 +18,14 @@ A soma de 1 até 3 é 6.
 Dica: Use o comando "for" junto com "range()" para percorrer os números,
 e uma variável para ir acumulando a soma.
 """
+
+
+
+numero = int(input("Digite um numero: "))
+
+soma = 0
+
+for i in range(1, numero + 1): #na função range, o número de término não é incluido. O +1 faz com que o número seja incluido
+    soma = soma + i
+
+print(soma)
